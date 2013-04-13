8eac165e5c57c0de93b002d201668ce1e99352ca
hortinstein <hortinstein@hortinstein-VirtualBox.(none)>
2013-04-13 18:42:53 +0900

    added logging, fixed post tests for config file

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 //swarmgent//
 
+log = require('./log/log.js'); //winston configuration
+
 var restify = require('restify');
 var server = restify.createServer();
-//server.use(restify.bodyParser({ mapParams: false }));
-server.use(restify.bodyParser());
-server.post('/init', function (req, res, next) {
-    console.log(req.params);
+server.use(restify.bodyParser({
+	mapParams: false
+}));
+
+//when swarmlicant is identified as a curator 
+var curator = function (config) {
+	log.info("curator initialized");
+}
+
+//when swarmlicant is identified as a trove 
+var trove = function (config) {
+	log.info("trove initialized");
+}
+
+server.post('/init', function(req, res, next) {
 	var config = req.body;
 	console.log(config);
-    res.send('ok')
+	switch(config.type){
+		case "curator":
+			curator(config);
+			break;
+		case "trove":
+			trove(config);
+			break;
+	};
+	res.send('ok');
 });
 
-
-server.get('/ping', function (req, res, next) {
-    res.send('pong');
+server.get('/ping', function(req, res, next) {
+	res.send('pong');
 });
 
-server.get('/config', function (req, res, next) {
-    //config = req.body;
-	//console.log(config);
+server.get('/config', function(req, res, next) {
+	config = req.body;
+	console.log(config);
 });
+
+server.get('/update', function(req, res, next) {
+	config = req.body;
+	console.log(config);
+});
+
 
 server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+	console.log('%s listening at %s', server.name, server.url);
 });
--- test/test.js
+++ test/test.js
@@ -3,18 +3,18 @@ var should = require('should');
 var request = require('request');
 var swarmgent = require('../index.js');
 
-it('should be able to ping the server', function (done) {
-  request('http://127.8.202.129:8080/ping', function (e,r,o){
-    o.should.equal("\"pong\"");
-    done();
-  });
+it('should be able to ping the server', function(done) {
+	request('http://127.8.202.129:8080/ping', function(e, r, o) {
+		o.should.equal("\"pong\"");
+		done();
+	});
 });
 
-it('should be able to recieve the curator configuration file', function (done) {
-    var config = require('./test_configs/curator_config.json');
-  request.post('http://127.8.202.129:8080/init',config,function(e,r,o){
-    o.should.equal("\"ok\"");
-    done();
-  });
-  
+it('should be able to recieve the curator configuration file', function(done) {
+	var config = require('./test_configs/curator_config.json');
+	request.post('http://127.8.202.129:8080/init', function(e, r, o) {
+		o.should.equal("\"ok\"");
+		done();
+	}).form(config);
+
 });
